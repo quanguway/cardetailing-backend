91fcdfd4d574562cb85aac56eb1eee94394ac096
hainamdev <hainamdev@gmail.com>
2023-03-18 20:16:15 +0700

    fix db

--- src/database/seeds/1_unit.ts
+++ src/database/seeds/1_unit.ts
@@ -1,36 +1,35 @@
 import { Knex } from "knex";
-import {faker} from '@faker-js/faker';
-
+import { faker } from "@faker-js/faker";
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
-    await knex("units").del();
-    
-    // title: string;
-    // 	description: string;
-    // 	date_created: faker.date.past();
-    // 	date_updated: Date;
-    // 	staff_updated: Date;
-        // staff_created: Date; 
-    // Inserts seed entries
-    for (let index = 0; index < 1; index++) {
-        await knex("units").insert([
-            // {
-            //     title: "lượt",
-            //     description: faker.lorem.paragraph(),
-            //     date_created: faker.date.past(),
-            //     date_updated: faker.date.past(),
-            //     staff_updated: faker.date.past(),
-            //     staff_created: faker.date.past(),
-            // },
-            {
-                title: "lượt",
-                description: 'Không có mô tả',
-                date_created:faker.date.past(),
-                date_updated: faker.date.past(),
-                staff_updated: faker.date.past(),
-                staff_created: faker.date.past(),
-            }
-        ]);
-    }
-};
+  // Deletes ALL existing entries
+  await knex("units").del();
+
+  // title: string;
+  // 	description: string;
+  // 	date_created: faker.date.past();
+  // 	date_updated: Date;
+  // 	staff_updated: Date;
+  // staff_created: Date;
+  // Inserts seed entries
+  for (let index = 0; index < 1; index++) {
+    await knex("units").insert([
+      // {
+      //     title: "lượt",
+      //     description: faker.lorem.paragraph(),
+      //     date_created: faker.date.past(),
+      //     date_updated: faker.date.past(),
+      //     staff_updated: faker.date.past(),
+      //     staff_created: faker.date.past(),
+      // },
+      {
+        title: "lượt",
+        description: "Không có mô tả",
+        date_created: faker.date.past(),
+        date_updated: faker.date.past(),
+        staff_updated: faker.date.past(),
+        staff_created: faker.date.past(),
+      },
+    ]);
+  }
+}
