45a5b3bdf80087f3ca7846c380a8dccc28a52a1e
hainamdev <hainamdev@gmail.com>
2023-03-27 22:01:19 +0700

    fix db

--- src/api/modules/Booking/booking.service.ts
+++ src/api/modules/Booking/booking.service.ts
@@ -2,6 +2,7 @@ import knex from "../../../database/knex";
 import { BookingDetail } from "../BookingDetail/bookingDetail";
 import { BookingDetailService } from "../BookingDetail/bookingDetail.service";
 import { CarDetailService } from "../CarDetail/carDetail.service";
+import { CustomerRepository } from "../Custommer/customer.repository";
 import { CustomerService } from "../Custommer/customer.service";
 import { ProductRepository } from "../Product/product.repository";
 import { PromotionDetailRepository } from "../PromotionDetail/promotionDetail.repository";
@@ -12,93 +13,117 @@ import { BookingDTO } from "./booking.dto";
 import { BookingRepository } from "./booking.repository";
 
 export class BookingService {
-    private readonly bookingRepository;
-    private readonly bookingDetailService;
-    private readonly customerService;
-    private readonly carDetailservice;
-    private readonly staffService;
-    private readonly slotService;
-    private readonly promotionDetailRepository;
-    private readonly productRepository;
+  private readonly bookingRepository;
+  private readonly bookingDetailService;
+  private readonly customerService;
+  private readonly carDetailservice;
+  private readonly staffService;
+  private readonly slotService;
+  private readonly promotionDetailRepository;
+  private readonly productRepository;
 
-    constructor() {
-        this.bookingRepository = new BookingRepository(knex, 'booking');
-        this.bookingDetailService = new BookingDetailService();
-        this.customerService = new CustomerService();
-        this.staffService = new StaffService();
-        this.carDetailservice = new CarDetailService();
-        this.slotService = new SlotService();
-        this.promotionDetailRepository = new PromotionDetailRepository(knex, `promotion_details`);
-        this.productRepository = new ProductRepository(knex, `products`);
+  constructor() {
+    this.bookingRepository = new BookingRepository(knex, "booking");
+    this.bookingDetailService = new BookingDetailService();
+    this.customerService = new CustomerRepository(knex, "customers");
+    this.staffService = new StaffService();
+    this.carDetailservice = new CarDetailService();
+    this.slotService = new SlotService();
+    this.promotionDetailRepository = new PromotionDetailRepository(
+      knex,
+      `promotion_details`
+    );
+    this.productRepository = new ProductRepository(knex, `products`);
+  }
+
+  // async getAll() {
+  //     const response = await this.bookingRepository.getAll();
+  //     const array: any = []
+  //     for(const element of response) {
+  //         const bookingDetails = await this.bookingDetailService.find({booking_id: element.id})
+  //         const customer = await this.customerService.findFirst({id: element.customer_id})
+  //         const dto = new BookingDTO(element, customer, bookingDetails as BookingDetail[]);
+  //         array.push({...dto})
+  //     }
+  //     return array;
+  // }
+
+  async findFirst(item: Booking) {
+    const book = await this.bookingRepository.findFirst(item);
+    const customer = await this.customerService.findFirst({
+      id: book.customer_id,
+    });
+
+    const bookDetails = await this.bookingDetailService.find({
+      booking_id: book.id,
+    });
+    const carDetail = await this.carDetailservice.find({
+      id: book.car_detail_id,
+    });
+
+    const dto = new BookingDTO(
+      book,
+      customer,
+      bookDetails as BookingDetail[],
+      carDetail[0]
+    );
+
+    return dto;
+  }
+
+  async update(id: string, item: Booking) {
+    return await this, this.bookingRepository.update(id, item);
+  }
+
+  async create(item: any) {
+    if (item.isNewCustomer) {
+      const customer = await this.customerService.create(item.customer);
+    }
+    if (item.isNewCar) {
+      const carDetail = await this.carDetailservice.create(item.car_detail);
     }
 
-    // async getAll() {
-    //     const response = await this.bookingRepository.getAll();
-    //     const array: any = []
-    //     for(const element of response) {
-    //         const bookingDetails = await this.bookingDetailService.find({booking_id: element.id})
-    //         const customer = await this.customerService.findFirst({id: element.customer_id})
-    //         const dto = new BookingDTO(element, customer, bookingDetails as BookingDetail[]);
-    //         array.push({...dto})
-    //     }
-    //     return array; 
-    // }
+    const booking = {
+      id: item.id,
+      customer_id: item.customer.id,
+      slot_id: item.slot_id,
+      car_detail_id: item.car_detail_id,
+      date_created: new Date(),
+      status: "WAITING",
+    };
 
-    async findFirst(item: Booking) {
-        
-        const book = await this.bookingRepository.findFirst(item);
-        const customer = await this.customerService.findFirst({id: book.customer_id});
-        
-        const bookDetails = await this.bookingDetailService.find({booking_id: book.id});
-        const carDetail = await this.carDetailservice.find({id: book.car_detail_id});
+    this.slotService.update(item.slot_id, {
+      is_empty: false,
+      booking_id: item.id,
+    });
 
-        
-        const dto = new BookingDTO(book, customer, bookDetails as BookingDetail[], carDetail[0]);
+    const bookingCreated = await this.bookingRepository.create(booking);
+    const bookingDetails = await this.bookingDetailService.createMany(
+      item.booking_details
+    );
+  }
 
-        return dto;
-    }  
+  async createWithOldCustomer(item: any) {
+    const booking = {
+      id: item.id,
+      customer_id: item.customer_id,
+      slot_id: item.slot_id,
+      status: "WAITING",
+    };
 
-    async update (id:string, item: Booking) {
-        return await this,this.bookingRepository.update(id, item)
-    }
+    this.slotService.update(item.slot_id, {
+      is_empty: false,
+      booking_id: item.id,
+    });
 
-    async create(item: any){
-        const customer = await this.customerService.create(item.customer)
-        const carDetail = await this.carDetailservice.create(item.car_detail);
- 
-        const booking = { 
-            id: item.id,
-            customer_id: item.customer.id,
-            slot_id: item.slot_id,
-            car_detail_id: item.car_detail_id,
-            date_created: new Date(),
-            status: 'WAITING'
-        } 
+    const bookingCreated = await this.bookingRepository.create(booking);
+    const bookingDetails = await this.bookingDetailService.createMany(
+      item.booking_details
+    );
+  }
 
-        this.slotService.update(item.slot_id, {is_empty: false, booking_id: item.id})
-
-        const bookingCreated = await this.bookingRepository.create(booking);
-        const bookingDetails = await this.bookingDetailService.createMany(item.booking_details)
- 
-    }
-
-    async createWithOldCustomer(item: any){
-        const booking = { 
-            id: item.id,
-            customer_id: item.customer_id,
-            slot_id: item.slot_id,
-            status: 'WAITING'
-        } 
-
-        this.slotService.update(item.slot_id, {is_empty: false, booking_id: item.id})
-
-        const bookingCreated = await this.bookingRepository.create(booking);
-        const bookingDetails = await this.bookingDetailService.createMany(item.booking_details)
- 
-    }
-
-    async delete(id: string) {
-        const response = await this.bookingRepository.delete(id);
-        return response;  
-    }
+  async delete(id: string) {
+    const response = await this.bookingRepository.delete(id);
+    return response;
+  }
 }
--- src/api/modules/Custommer/customer.service.ts
+++ src/api/modules/Custommer/customer.service.ts
@@ -52,18 +52,33 @@ export class CustomerService {
   }
 
   async findFirst(item: Customer) {
-    const customer = await this.customerRepository.findFirst(item);
+    try {
+      const customer = await this.customerRepository.findFirst(item);
+      const address = this.addressService.getNodeById(
+        customer.address_id as string
+      );
+      const addressPathTitles = this.addressService.getPathByTitle(
+        address.title
+      );
+      const addressPaths = this.addressService.getPathById(
+        customer.address_id as string
+      );
+      const addressPathsCustom = addressPaths;
+      const addressPathTitlesCustom = addressPathTitles.slice(
+        addressPathTitles.indexOf("/") + 1
+      );
+      const role = await this.roleService.findById(customer.role_id);
 
-    // const address = this.addressService.getNodeById(customer.address_id as string);
-    // const addressPathTitles = this.addressService.getPathByTitle(address.title);
-    // const addressPaths = this.addressService.getPathById(customer.address_id as string)
-
-    // const addressPathsCustom = addressPaths;
-    // const addressPathTitlesCustom = addressPathTitles.slice(addressPathTitles.indexOf('/') + 1);
-
-    // const role = await this.roleService.findById(customer.role_id);
-    // const dto = new CustomerDTO(customer, role, addressPathsCustom.split('.'), addressPathTitlesCustom);
-    return customer;
+      const dto = new CustomerDTO(
+        customer,
+        role,
+        addressPathsCustom.split("."),
+        addressPathTitlesCustom
+      );
+      return dto;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async update(id: string, item: any) {
@@ -86,8 +101,9 @@ export class CustomerService {
     }
     // const staff = item.map(({address, ...orther}:{address:any}) => orther)
     delete item.address;
-    (item.role_id = "c7f0ac26-ae1d-11ed-afa1-0242ac120002"),
-      (item.address_id = address?.id ?? addressId);
+    item.role_id = "c7f0ac26-ae1d-11ed-afa1-0242ac120002";
+    item.address_id = address?.id ?? addressId;
+    console.log(item);
     return await this.customerRepository.create(item);
   }
 
